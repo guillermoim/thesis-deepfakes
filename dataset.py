00ef145068b32bd1b7b63425a7d366b6a6f049fd
guillermoim <guiinfmol@yahoo.com>
2020-06-16 12:52:29 +0200

    Major changes

--- dataset.py
+++ dataset.py
@@ -1,8 +1,84 @@
-import torch,os, math
+import torch, os, math
 import random
 from PIL import Image
 import torchvision.transforms as transforms
 import csv
+import torchvision
+import random
+from PIL import Image, ImageDraw
+import dlib
+import numpy as np
+from skimage.metrics import structural_similarity as ssim
+from albumentations import *
+import cv2
+
+
+regions = {  'left'     :   [0, 1, 2, 3, 4, 5, 6, 7, 8, 30, 29, 28, 27, 21, 20, 19, 18, 17],
+             'right'    :   [16, 15, 14, 13, 12, 11, 10, 9, 8, 30, 29, 28, 27, 22, 23, 24, 25, 26],
+             'bottom'   :   [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+             'nose'     :   [31, 32, 33, 34, 35, 23, 21],
+             'eyes'     :   None,
+             'mouth'    :   None,
+             'top'      :   None,
+         }
+
+T = torchvision.transforms.ToTensor()
+
+class ImbalancedDatasetSampler(torch.utils.data.sampler.Sampler):
+    """Samples elements randomly from a given list of indices for imbalanced dataset
+    Arguments:
+        indices (list, optional): a list of indices
+        num_samples (int, optional): number of samples to draw
+        callback_get_label func: a callback-like function which takes two arguments - dataset and index
+    """
+
+    def __init__(self, dataset, indices=None, num_samples=None, callback_get_label=None):
+                
+        # if indices is not provided, 
+        # all elements in the dataset will be considered
+        self.indices = list(range(len(dataset))) \
+            if indices is None else indices
+
+        # define custom callback
+        self.callback_get_label = callback_get_label
+
+        # if num_samples is not provided, 
+        # draw `len(indices)` samples in each iteration
+        self.num_samples = len(self.indices) \
+            if num_samples is None else num_samples
+            
+        # distribution of classes in the dataset 
+        label_to_count = {}
+        for idx in self.indices:
+            label = self._get_label(dataset, idx)
+            if label in label_to_count:
+                label_to_count[label] += 1
+            else:
+                label_to_count[label] = 1
+                
+        # weight for each sample
+        weights = [1.0 / label_to_count[self._get_label(dataset, idx)]
+                   for idx in self.indices]
+        self.weights = torch.DoubleTensor(weights)
+
+    def _get_label(self, dataset, idx):
+        if isinstance(dataset, torchvision.datasets.MNIST):
+            return dataset.train_labels[idx].item()
+        elif isinstance(dataset, torchvision.datasets.ImageFolder):
+            return dataset.imgs[idx][1]
+        elif isinstance(dataset, torch.utils.data.Subset):
+            return dataset.dataset.imgs[idx][1]
+        elif self.callback_get_label:
+            return self.callback_get_label(dataset, idx)
+        else:
+            raise NotImplementedError
+                
+    def __iter__(self):
+        return (self.indices[i] for i in torch.multinomial(
+            self.weights, self.num_samples, replacement=True))
+
+    def __len__(self):
+        return self.num_samples
 
 class RandomDataset(torch.utils.data.IterableDataset):
 
@@ -52,14 +128,169 @@ class BalancedClusterDataset(torch.utils.data.IterableDataset):
             iter_end = min(iter_start + per_worker, len(faces) - 1)
 
         return map(lambda x: format_row(x), faces[iter_start:iter_end])
+    
 
+class AugmentedDataset(torch.utils.data.Dataset):
 
-def format_row(item):
-    img = Image.open(item[-1])
+    def __init__(self, path, path_data):
+        super(AugmentedDataset).__init__()
+        f_p = os.path.abspath(path)
+        assert os.path.exists(f_p), "The directory does not exist"
+        f_p_data = os.path.abspath(path_data)
+        assert os.path.exists(f_p_data), "The directory does not exist"
+        self.f_p = f_p
+        self.f_p_data = f_p_data
+        self.augment = create_train_transforms()
+        
+        with open(self.f_p) as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            rows = [row for row in reader][1:]
+            self.rows = rows
+
+    def __len__(self):
+        return len(self.rows)
+
+    def __getitem__(self, idx):
+        video, frame, original_frame, n_face, order, lands, label = self.rows[idx]
+        res = oclude_frame(f'{self.f_p_data}/{frame}', f'{self.f_p_data}/{original_frame}', float(label), eval(lands))
+        if random.random() > 0.5:
+            res = self.augment(image=res)['image']
+        return T(res), torch.tensor(float(label))
+    
+    
+def oclude_frame(frame, frame_original, label, lands, p=0.5):
+    res = None
+    if label > 0.5:
+        if random.random() > 0.5:
+            res = np.array(make_occlusion_fake(frame, frame_original, lands))
+        else:
+            res = center_image(np.array(Image.open(frame)), lands)
+    else:
+        if random.random() > 0.5:
+            res = np.array(make_occlusion_original(frame, lands))
+        else:
+            res = center_image(np.array(Image.open(frame)), lands)
+    
+    return res
+    
+def create_train_transforms(size=224):
+    # Augmentations with albumentations
+    return Compose([
+       ImageCompression(quality_lower=60, quality_upper=100, p=0.5),
+       GaussNoise(p=0.1),
+       GaussianBlur(blur_limit=3, p=0.05),
+       HorizontalFlip(),
+       #IsotropicResize(max_side=size),
+       PadIfNeeded(min_height=size, min_width=size, border_mode=cv2.BORDER_CONSTANT),
+       OneOf([RandomBrightnessContrast(), FancyPCA(), HueSaturationValue()], p=0.7),
+       ToGray(p=0.2),
+       ShiftScaleRotate(shift_limit=0.1, scale_limit=0.2, rotate_limit=10, border_mode=cv2.BORDER_CONSTANT, p=0.5),
+    ])
+
+def make_occlusion_fake(frame, original, lands):
+    
+    av = True
+    done = []
+    
+
+    forged = Image.open(frame)
+    pristine = Image.open(original)
+    
+    pristine_np = np.array(pristine)
+    forged_np = np.array(forged)
+    
+    score, score_mask = ssim(pristine_np, forged_np, multichannel=True, full=True)
+    diff = ((1 - score_mask) * 255).astype(np.uint8)
+
+    poly_ = None
+    
+    while av:
+        
+        diff_im = Image.fromarray(diff)
+        draw = ImageDraw.Draw(diff_im)
+    
+        part = random.choice(list(regions.keys()))
+        
+        if part in done:
+            continue
+        done.append(part)
+        if len(done) == len(regions): av = False
+        # TODO: Wrap this in another function shared with original one!
+        if part == 'eyes':
+            poly = [(lands[36][0]-10, lands[36][1] + 20), (lands[36][0]-10, lands[36][1] - 20), (lands[45][0]+10, lands[45][1] - 20), (lands[45][0]+10, lands[45][1] + 20)]
+        elif part == 'mouth':
+            poly = [(lands[48][0]-5, lands[48][1] + 15), (lands[48][0]-5, lands[48][1] - 15), (lands[54][0]+5, lands[54][1] - 15), (lands[54][0]+5, lands[54][1] + 15)]
+        elif part == 'top':
+            y = max((lands[29][1], lands[41][1], lands[46][1]))
+            poly = [(0, 0), (224, 0), (224, y), (0, y)]
+        else:
+            poly = [tuple(lands[v]) for v in regions[part]]
+        
+        draw.polygon(poly, fill='black')
+        
+        mask_edited = np.array(diff_im)
+
+        mean_original = np.mean(diff)
+        mean_edited = np.mean(mask_edited)
+                
+        if mean_edited < mean_original * 0.75:
+            #refuse change
+            continue
+        else:
+            poly_ = poly
+            av = False
+    
+    if poly_ is not None:
+        draw = ImageDraw.Draw(forged)
+        draw.polygon(poly_, fill='black')
+        
+    pic = T(forged) * 255
+    pic = pic.permute(1,2,0).to(torch.uint8).numpy()
+    
+    return center_image(pic, lands)
+
+def make_occlusion_original(frame, lands):
+    
+    img = Image.open(frame)
+    
+    draw = ImageDraw.Draw(img)
+
+    part = random.choice(list(regions.keys()))
+    
+    if part == 'eyes':
+        poly = [(lands[36][0]-10, lands[36][1] + 20), (lands[36][0]-10, lands[36][1] - 20), (lands[45][0]+10, lands[45][1] - 20), (lands[45][0]+10, lands[45][1] + 20)]
+    elif part == 'mouth':
+        poly = [(lands[48][0]-5, lands[48][1] + 15), (lands[48][0]-5, lands[48][1] - 15), (lands[54][0]+5, lands[54][1] - 15), (lands[54][0]+5, lands[54][1] + 15)]
+    elif part == 'top':
+        y = max((lands[29][1], lands[41][1], lands[46][1]))
+        poly = [(0, 0), (224, 0), (224, y), (0, y)]
+    else:
+        poly = [tuple(lands[v]) for v in regions[part]]
+        
+    draw.polygon(poly, fill='black')
+    pic = T(img) * 255
+    pic = pic.permute(1,2,0).to(torch.uint8).numpy()
+    
+    return center_image(pic, lands)
+    
+    
+def center_image(image, lands):
+    '''Input: Image in numpy and points'''
+    box = dlib.rectangle(0,0,224,224)
+    f_o_ds = dlib.full_object_detections()
+    points = [dlib.point(np.array(point)) for point in lands]
+    f_o_d = dlib.full_object_detection(box, points)
+    f_o_ds.append(f_o_d)
+    images = dlib.get_face_chips(image, f_o_ds, size=224, padding=0.4)
+    return images[0]       
+    
+
+def format_row(item, f_p_data):
+    img = Image.open(f'{f_p_data}/{item[-2]}')
     # transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=[0.5, 0.5, 0.5],std=[0.5, 0.5, 0.5])])
     # transform = transforms.Compose([transforms.Resize(224), transforms.ToTensor(), transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),])
     transform = transforms.ToTensor()
     # TODO: Should normalization be removed?
     # If needed for regression the negative label can be changed here instead of in the middle of the running code
-    label = int(item[1])
-    return transform(img), torch.tensor(label)
+    label = int(item[-3])
+    return transform(img), torch.tensor(int(label))
--- face_extraction.py
+++ face_extraction.py
@@ -3,20 +3,22 @@ from facenet_pytorch import MTCNN
 import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-
+import face_alignment
 
 def overlapping(low_1, high_1, low_2, high_2):
     return max(0, min(high_1, high_2) - max(low_1, low_2))
     
 
-def extract_faces_from_dir(src_dir, dst_dir):
+def extract_real_faces_from_dir(src_dir, dst_dir):
     
     # Init face detection net
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') 
     mtcnn = MTCNN(image_size=224, thresholds=[.98, .98, .99], margin=30, post_process=False, select_largest=True, keep_all=True, device=device)
-    
+
     to_pil_im = torchvision.transforms.ToPILImage()
     resize = torchvision.transforms.Resize((224, 224))   
+    
+    fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, flip_input=False)
 
     assert os.path.exists(src_dir), f'Source dir {src_dir} does not exist'
     if os.path.exists(dst_dir):
@@ -33,19 +35,22 @@ def extract_faces_from_dir(src_dir, dst_dir):
     metadata_out = {}
 
     for video in tqdm(videos):
+        
+        if videos[video]['label'] == 'FAKE':
+            continue
 
         video_name = video.split('.')[0]
-        label = 0 if videos[video]['label'] == 'REAL' else 1
+        label = 0
 
         video_path = f'{src_fp}/{video}'
-        vframes, _, info = torchvision.io.read_video(video_path, start_pts=2, end_pts=3.02, pts_unit='sec')
+        vframes, _, _ = torchvision.io.read_video(video_path, start_pts=2, end_pts=3.5, pts_unit='sec')
+        vframes = vframes[:32, :, :, :]
 
         # First detected surrounding boxes as well as probabilities and landmarks points.
         with torch.no_grad():
-            all_boxes, probs, all_points = mtcnn.detect(vframes.numpy(), landmarks=True)
+            all_boxes, probs = mtcnn.detect(vframes.numpy(), landmarks=False)
         
         all_boxes = np.array([v for v in all_boxes if v is not None])
-        all_points = np.array([p for p in all_points if p is not None])
         
         num_faces = max([v.shape[0] for v in all_boxes if v is not None] + [0])
 
@@ -54,7 +59,7 @@ def extract_faces_from_dir(src_dir, dst_dir):
             continue
 
         # Scale factor
-        scale_factor = 0.25
+        scale_factor = 0.45
 
         # x and y lengths
         max_x = vframes[0].size()[0]
@@ -62,17 +67,20 @@ def extract_faces_from_dir(src_dir, dst_dir):
 
         # create the dict that stores the images
         faces_in_video = {x: [] for x in range(num_faces)}
+        boxes_coordinates = {x: [] for x in range(num_faces)}
+        landmarks_coordinates = {x: [] for x in range(num_faces)}
+
 
         # auxiliary list for box ordering
         last_set_boxes = [None for _ in range(num_faces)]
 
         # iterate over the frames, extract sequences of images and save.
-        for ii, (boxes, points, frame) in enumerate(zip(all_boxes, all_points, vframes)):  
+        for ii, (boxes, frame) in enumerate(zip(all_boxes, vframes)):  
                         
-            for ij, (box, point) in enumerate(zip(boxes, points)):
+            for ij, box in enumerate(boxes):
 
                 box = list(map(int, box))
-                landmarks = [(int(p[0] - box[0]), int(p[1] - box[1])) for p in point]
+                #landmarks = [(int(p[0] - box[0]), int(p[1] - box[1])) for p in point]
 
                 len_x = abs(box[1] - box[3])
                 len_y = abs(box[0] - box[2])
@@ -80,10 +88,13 @@ def extract_faces_from_dir(src_dir, dst_dir):
                 scale_x = int(len_x * scale_factor // 2)
                 scale_y = int(len_y * scale_factor // 2)
 
-                face = frame[max(0, box[1]-scale_x):min(box[3]+scale_x, max_x), max(0,box[0]-scale_y):min(max_y, box[2]+scale_y), :]
+                box_ = x_min, x_max, y_min, y_max = max(0, box[1]-scale_x), min(box[3]+scale_x, max_x), max(0,box[0]-scale_y),min(max_y, box[2]+scale_y)
+                face = frame[x_min:x_max, y_min:y_max, :]
                 face = to_pil_im(face.permute(2, 0, 1))
                 face = resize(face)
-
+                
+                landmarks = fa.get_landmarks(np.array(face))
+                                                
                 box_area = abs(box[0] - box[2]) * abs(box[1] - box[3])
 
                 # Do the tracking to correcly construct the sequences            
@@ -92,13 +103,13 @@ def extract_faces_from_dir(src_dir, dst_dir):
 
                     overlappings_x = [overlapping(box[0], box[2], b[0], b[2]) for b in last_set_boxes if b is not None]
                     overlappings_y = [overlapping(box[1], box[3], b[1], b[3]) for b in last_set_boxes if b is not None]
-
                     overlapped_areas = [x*y / (1.0*box_area) for x,y in zip(overlappings_x, overlappings_y)]
-
                     indx = np.argmax(overlapped_areas)
 
                 last_set_boxes[indx] = box
                 faces_in_video[indx].append(face)
+                boxes_coordinates[indx].append(box_)
+                landmarks_coordinates[indx].append(landmarks[0].tolist())
 
         cnt = 0
 
@@ -117,13 +128,76 @@ def extract_faces_from_dir(src_dir, dst_dir):
                 cnt+=1
                 
         
-        metadata_out[video] = {'label':label, 'num_faces':cnt, 'faces':out_pic_list}
+        metadata_out[video] = {'label':label, 'num_faces':cnt, 'faces':out_pic_list, 'boxes': boxes_coordinates, 'landmarks':landmarks_coordinates, 'original':video}
                 
-    json.dump(metadata_out, open(f'{dst_dir}/metadata.json', 'w+'))                
+    json.dump(metadata_out, open(f'{dst_dir}/metadata_reals.json', 'w+'))                
 
     del mtcnn
+    
+
+def extract_fake_faces_from_dir(src_dir, dst_dir):
+    
+    to_pil_im = torchvision.transforms.ToPILImage()
+    resize = torchvision.transforms.Resize((224, 224))
+    
+    assert os.path.exists(src_dir), f'Source directory {src_dir} does not exist'
+    assert os.path.exists(src_dir), f'Destination directory {dst_dir} does not exist'
+
+    src_fp = os.path.abspath(src_dir)
+    dst_fp = os.path.abspath(dst_dir)
+
+    real_videos = json.load(open(f'{dst_dir}/metadata_reals.json'))
+    videos = json.load(open(f'{src_fp}/metadata.json'))
+    
+    fake_videos = {video: videos[video] for video in videos if videos[video]['label']=='FAKE'}
+    
+    metadata_out = {key:real_videos[key] for key in real_videos}
+        
+    
+    for video in tqdm(real_videos):
+                
+        vid_fakes = {f_video: fake_videos[f_video] for f_video in fake_videos if fake_videos[f_video]['original']==video}
+                
+        boxes = real_videos[video]['boxes']
+        landmarks_coordinates = real_videos[video]['landmarks']
+
+        for video_fake in vid_fakes:
+                        
+            video_name = video_fake.split('.')[0]
+            label = 1
+            
+            video_path = f'{src_fp}/{video_fake}'
+            vframes, _, _ = torchvision.io.read_video(video_path, start_pts=2, end_pts=3.5, pts_unit='sec')
+            vframes = vframes[:32, :, :, :]
+                        
+            out_pic_list = {}
+            
+            for i in boxes:
+                aux_list = [] # auxiliary list to store names of pictures
+                for idx, (box, frame) in enumerate(zip(boxes[i], vframes)):
+                    x_min, x_max, y_min, y_max = box
+                    face = frame[x_min:x_max, y_min:y_max, :]
+                    face = to_pil_im(face.permute(2, 0, 1))
+                    face = resize(face)
+                    file_name = f'{video_name}_{i}_{idx}.png'
+                    aux_list.append(file_name)
+                    face.save(f'{dst_fp}/{file_name}')
+                
+                out_pic_list[i] = aux_list
+                            
+            metadata_out[video_fake] = {'label':label, 'num_faces':len(boxes), 'faces':out_pic_list, 'original':video, 'landmarks':landmarks_coordinates}
+    
+    json.dump(metadata_out, open(f'{dst_dir}/metadata.json', 'w+'))       
 
 if __name__ == '__main__':
     
-    for i in range(0,10):
-        extract_faces_from_dir(f'data/dfdc_train_part_{i}', f'data/faces/chunk{i}')
+    a = 0; b = 10
+    print(f'Extracting all real faces from {a} to {b}')
+    for i in range(a,b):
+        break
+        extract_real_faces_from_dir(f'data/dfdc_train_part_{i}', f'data/faces/chunk{i}')
+    
+    print(f'Extracting all fake faces from {a} to {b}')
+    for i in range(a,b):
+        extract_fake_faces_from_dir(f'data/dfdc_train_part_{i}', f'data/faces/chunk{i}')
+        break

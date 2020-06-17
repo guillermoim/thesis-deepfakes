96e76316b3596225db6ecad3f9feb6c4c19f1bed
guillermoim <guiinfmol@yahoo.com>
2020-06-17 17:56:02 +0200

    Last modified.

--- exec_single_2.py
+++ exec_single_2.py
@@ -11,19 +11,23 @@ if __name__ == '__main__':
 
     model.to(device)
 
+
+
     if torch.cuda.device_count() > 1:
         print("Let's use", torch.cuda.device_count(), "GPUs!")
 
     model = torch.nn.DataParallel(model)
 
     dataset = ds.AugmentedDataset('datasets/full_train_dataset.csv', 'data/faces/')
-    print('\t\t Training Details')
-    print('------------------------------------')
+
     exec_name = 'EffNet-B7'
     epochs = 5
     batch_size = 48
     epoch_size = 200
     power = 1
+
+    print('\t\t Training Details')
+    print('------------------------------------')
 
     print(f'Execution name: {exec_name}')
     print(f'Epochs: {epochs}')
@@ -35,4 +39,6 @@ if __name__ == '__main__':
     print('\t\t Epoch information - Single Model')
     print('------------------------------------')
     ns.train_var_2(exec_name, model, dataset, epochs, batch_size, epoch_size, power, device)
+
+    path_to_save = 'models/effnet_b7-v2.pth'
     torch.save(model.state_dict(), path_to_save)

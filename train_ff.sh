5b0825486160c879f283aa5ff3671e2ba5b9ff2d
guillermo <guiinfmol@yahoo.com>
2020-08-12 10:44:48 +0000

    Few changes, requirements.txt updated

--- train_ff.sh
+++ train_ff.sh
@@ -1,9 +1,18 @@
 source ../torch/bin/activate
 
-PYTHONPATH=. CUDA_VISIBLE_DEVICES="1,2,3,4,5,6" python -m torch.distributed.launch --nproc_per_node=6 train_ff.py --ngpu 6  --distributed --n_workers 6 > outputs/effnetb3_tum_101_v0_fullDA.txt
-PYTHONPATH=. CUDA_VISIBLE_DEVICES="1,2,3,4,5,6" python -m torch.distributed.launch --nproc_per_node=6 train_ff.py --ngpu 6  --data_augment no_da --distributed --n_workers 6 > outputs/effnetb3_tum_101_v0_noDA.txt
-PYTHONPATH=. CUDA_VISIBLE_DEVICES="1,2,3,4,5,6" python -m torch.distributed.launch --nproc_per_node=6 train_ff.py --ngpu 6  --data_augment simple_da --distributed --n_workers 6 > outputs/effnetb3_tum_101_v0_simpleDA.txt
-PYTHONPATH=. CUDA_VISIBLE_DEVICES="1,2,3,4,5,6" python -m torch.distributed.launch --nproc_per_node=6 train_ff.py --ngpu 6  --data_augment occlusions_da --distributed --n_workers 6 > outputs/effnetb3_tum_101_v0_occDA.txt
+#EfficientNet-B3
+
+#PYTHONPATH=. CUDA_VISIBLE_DEVICES="1,2,3,4,5,6" python -m torch.distributed.launch --nproc_per_node=6 train_ff.py --ngpu 6  --distributed --n_workers 6 > outputs/effnetb3_tum_101_v0_fullDA.txt
+#PYTHONPATH=. CUDA_VISIBLE_DEVICES="1,2,3,4,5,6" python -m torch.distributed.launch --nproc_per_node=6 train_ff.py --ngpu 6  --data_augment no_da --distributed --n_workers 6 > outputs/effnetb3_tum_101_v0_noDA.txt
+#PYTHONPATH=. CUDA_VISIBLE_DEVICES="1,2,3,4,5,6" python -m torch.distributed.launch --nproc_per_node=6 train_ff.py --ngpu 6  --data_augment simple_da --distributed --n_workers 6 > outputs/effnetb3_tum_101_v0_simpleDA.txt
+#PYTHONPATH=. CUDA_VISIBLE_DEVICES="1,2,3,4,5,6" python -m torch.distributed.launch --nproc_per_node=6 train_ff.py --ngpu 6  --data_augment occlusions_da --distributed --n_workers 6 > outputs/effnetb3_tum_101_v0_occDA.txt
+
+#EfficientNet-B5
+PYTHONPATH=. CUDA_VISIBLE_DEVICES="1,2,3,4,5,6" python -m torch.distributed.launch --nproc_per_node=6 train_ff.py --ngpu 6 --model efficientnet-b5 --batch_size 6 --distributed --n_workers 6 > outputs/effnetb5_tum_101_v0_fullDA.txt
+PYTHONPATH=. CUDA_VISIBLE_DEVICES="1,2,3,4,5,6" python -m torch.distributed.launch --nproc_per_node=6 train_ff.py --ngpu 6 --model efficientnet-b5 --batch_size 6 --data_augment no_da --distributed --n_workers 6 > outputs/effnetb5_tum_101_v0_noDA.txt
+PYTHONPATH=. CUDA_VISIBLE_DEVICES="1,2,3,4,5,6" python -m torch.distributed.launch --nproc_per_node=6 train_ff.py --ngpu 6 --model efficientnet-b5 --batch_size 6 --data_augment simple_da --distributed --n_workers 6 > outputs/effnetb5_tum_101_v0_simpleDA.txt
+PYTHONPATH=. CUDA_VISIBLE_DEVICES="1,2,3,4,5,6" python -m torch.distributed.launch --nproc_per_node=6 train_ff.py --ngpu 6 --model efficientnet-b5 --batch_size 6 --data_augment occlusions_da --distributed --n_workers 6 > outputs/effnetb5_tum_101_v0_occDA.txt
+
 
 #PYTHONPATH=. CUDA_VISIBLE_DEVICES="1,2,3,4,5,6" python -m torch.distributed.launch --nproc_per_node=6 train_ff.py -s 101 -m xception -batch_size 8 -epoch_size 100 -epochs 30 --ngpu 6 --n_workers 6 -v 0 --amp True --data_path ../datasets/mtcnn > outputs/xception_tum_101_v0.txt
 #PYTHONPATH=. CUDA_VISIBLE_DEVICES="1,2,3,4,5,6" python -m torch.distributed.launch --nproc_per_node=6 train_ff.py -s 101 -m efficientnet-b3 -batch_size 8 -epoch_size 100 -epochs 30 --ngpu 6 --n_workers 6 -v 0 --amp True --data_path ../datasets/mtcnn > outputs/effnetb3_tum_101_v0.txt
